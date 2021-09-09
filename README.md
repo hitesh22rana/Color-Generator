808c85ec02ec82a2c09545ba963a29d35420adfb
hitesh <hitesh22rana@gmail.com>
2021-09-09 21:02:44 +0530

    updated UI/UX

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Color-Generator
+<h1> Color Generator 🎨</h1>
+<br>
+
+<ul>
+    <li>Color-Generator that works on your browser with great UI/UX.</li>
+</ul>
--- script.js
+++ script.js
@@ -4,48 +4,56 @@ let blue = document.getElementById("Blue");
 let bodybg = document.getElementById("body-bg");
 
 
+red.oninput = () => {
+    if(red.value > 255) {
+        red.value = Math.floor(red.value / 10);
+        red.innerHTML = red.value;
+    }
+    if(green.value == "") {
+        green.value = 0
+        green.innerHTML = green.value;
+    }
+
+    if(blue.value == "") {
+        blue.value = 0
+        blue.innerHTML = blue.value;
+    }
+
+    bodybg.style.backgroundColor = 'rgb(' + red.value + ',' + green.value + ',' + blue.value + ')';
+};
+
 green.oninput = () => {
     if(green.value > 255) {
         green.value = Math.floor(green.value / 10);
         green.innerHTML = green.value;
     }
     if(red.value == "") {
-        console.log("hey");
+        red.value = 0
+        red.innerHTML = red.value;
+    }
+
+    if(blue.value == "") {
+        blue.value = 0
+        blue.innerHTML = blue.value;
     }
 
     bodybg.style.backgroundColor = 'rgb(' + red.value + ',' + green.value + ',' + blue.value + ')';
 };
 
+blue.oninput = () => {
+    if(blue.value > 255) {
+        blue.value = Math.floor(blue.value / 10);
+        blue.innerHTML = blue.value;
+    }
+    if(red.value == "") {
+        red.value = 0
+        red.innerHTML = red.value;
+    }
 
+    if(green.value == "") {
+        green.value = 0
+        green.innerHTML = green.value;
+    }
 
-// red.oninput = () => {
-//     if(red.value > 255 || green.value > 255 || blue.value > 255) {
-//         bodybg.style.backgroundColor = "white";
-//     }
-
-//     else {
-//         bodybg.style.backgroundColor = 'rgb(' + red.value + ',' + green.value + ',' + blue.value + ')';
-//     }
-// };
-    
-// green.oninput = () => {
-//     if(red.value > 255 || green.value > 255 || blue.value > 255) {
-//         bodybg.style.backgroundColor = "white";
-//     }
-
-//     else {
-//         bodybg.style.backgroundColor = 'rgb(' + red.value + ',' + green.value + ',' + blue.value + ')';
-//     }
-// };
-    
-// blue.oninput = () => {
-//     if(red.value > 255 || green.value > 255 || blue.value > 255) {
-//         bodybg.style.backgroundColor = "white";
-//     }
-
-//     else {
-//         bodybg.style.backgroundColor = 'rgb(' + red.value + ',' + green.value + ',' + blue.value + ')';
-//     }
-// };
-
-
+    bodybg.style.backgroundColor = 'rgb(' + red.value + ',' + green.value + ',' + blue.value + ')';
+}
